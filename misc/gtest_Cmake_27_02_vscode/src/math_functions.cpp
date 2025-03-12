ef226052209c98bea044a1fccb64ce5872a57384
taras <luviq7@gmail.com>
2025-03-12 17:31:39 +0200

    final_build

--- misc/gtest_Cmake_27_02_vscode/src/math_functions.cpp
+++
@@ -1,9 +0,0 @@
-#include "math_functions.h"
-
-int Sum(int a, int b) {
-    return a + b;
-}
-
-int Multiply(int a, int b) {
-    return a * b;
-}
--- misc/gtest_Cmake_27_02_vscode/src/math_functions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-//#ifndef MATH_FUNCTIONS_H
-//#define MATH_FUNCTIONS_H
-
-int Sum(int a, int b);
-int Multiply(int a, int b);
-
-//#endif // MATH_FUNCTIONS_H
